55dda39fd5e68603567a75bc68ff0ef3e31dd0a7
Finlay MacLennan <F.MacLennan@albynschool.org>
2025-03-12 15:35:20 +0000

    hih

---
+++ N5 Programs (Python)/Python Tasks/loops2.py
@@ -0,0 +1,10 @@
+# Program 2 - Predict and read
+
+message = input("please enter the message to display:")
+
+count = 0
+
+while count <= 10:
+    print(message)
+
+count += 1
---
+++ N5 Programs (Python)/Python Tasks/loops5.py
@@ -0,0 +1,9 @@
+#This program is a simulation of an ice hockey scoreboard.
+
+home = 0
+away = 0
+period = 1
+
+while period <= 3:
+    print("")
+    print
