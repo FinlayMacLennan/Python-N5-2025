d5f076788a0a9a2ca7e86503fb16582ca5a54401
Finlay MacLennan <F.MacLennan@albynschool.org>
2025-03-19 15:04:40 +0000

    hi

--- N5 Programs (Python)/Python Tasks/Predefined Functions Project.py
+++ N5 Programs (Python)/Python Tasks/Predefined Functions Project.py
@@ -2,11 +2,18 @@
 
 #From the code starter provided above, create a word guessing game that gives the user five turns to guess the secret word. 
 #Their score is then displayed.
-#The score for guessing correctly is the number of letters in the secret word divided by the number of turns taken to guess it, 
-#rounded to 1 decimal place. They score 0 if they run out of turns.
 
 import random
 
 words = ["python", "function", "random", "length", "computer", "program", "variable"]
 
 secret_word = random.choice(words)
+
+guess = input("Guess the word")
+
+if guess == secret_word:
+    print("Well Done you have guesses the word")
+    socre == len(secret_word)
+
+elif guess != secret_word:
+    print("try again that is not the right word")
