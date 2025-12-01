cbe4bb016176b83bb39141fadfedc576df2aa818
Finlay MacLennan <F.MacLennan@albynschool.org>
2025-12-01 11:39:48 +0000

    hi

--- N5 Programs (Python)/2019 Practice Assignment/2019 Question 2b.py
+++ N5 Programs (Python)/2019 Practice Assignment/2019 Question 2b.py
@@ -1,3 +1,6 @@
+import random
+
+final_usernames = []
 endings = ["ing","end","axe","gex","goh"]
 
 StudentsNo = int(input("Please enter the number of stuents that you need usernames generated for"))
@@ -6,8 +9,13 @@ for i in range(StudentsNo):
     name = input("Please enter the first 3 letters of the students name")
 
     while len(name) != 3:
-        print("Please enter 3 characters only")
+        print("Error, Please enter 3 characters only")
         name = input("Please enter the first 3 letters of the students name")
     print("Thank you for entering the first 3 letters of the students name")
 
-    username = (name, )
+    end = random.choice(endings)
+    username = (name, end)
+
+    final_usernames.append(username)
+
+print(final_usernames) 
