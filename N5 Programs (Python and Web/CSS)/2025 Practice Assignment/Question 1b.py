740fea89fc4336fa2c8800490bd9caf17e88b069
Finlay MacLennan <F.MacLennan@albynschool.org>
2026-02-03 09:47:19 +0000

    hi

--- N5 Programs (Python and Web/CSS)/2025 Practice Assignment/Question 1b.py
+++ N5 Programs (Python and Web/CSS)/2025 Practice Assignment/Question 1b.py
@@ -1,11 +1,30 @@
-fruits = ["apple","bananna","blueberry","kiwi","mango","orange","peach","pineapple","raspberry","strawberry"]
+import random
+
+counter = 0
+mystory_fruits = ["apple","bananna","blueberry","kiwi","mango","orange","peach","pineapple","raspberry","strawberry"]
 user_fruits = []
+decision = "Y"
 
-number_of_fruits = int(input("Please enter the number of fruits you want"))
+while decision == "Y" and counter < 6:
+    fruits = str(input("Please enter your choice of fruit"))
+    while len(fruits) <= 4:
+        print("invalid fruit, please enter a fruit with more then 4 characters")
+        fruits = str(input("Please enter your choice of fruit"))
+    counter = counter + 1
+    user_fruits.append(fruits)
+    decision = str(input("Do you want to enter another fruit? - Please enter Y/N"))
 
-for index in range(number_of_fruits):
-    choice = str(input("Please enter each fruit"))
-    while len(choice) < 4:
-        print("Invaild choice, fruits must be equal to or more then 4")
-        choice = str(input("Please enter each fruit"))
-user_fruits.append(choice)
+number =  random.randint(0,9)
+print("The fruits you entered were")
+print(user_fruits)
+print("The mystery fruit was")
+print(mystory_fruits[number])
+
+counter = counter + 1
+
+if counter < 3:
+    print("You have a milkshake")
+elif counter == 3 or counter == 4:
+    print("you have a smoothie")
+else:
+    print("You have a fruit juice")
